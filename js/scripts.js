f407dad00a4024de4f3ac042958c88eb1685924c
Alex Larson and Wade Grubbs <wadegrubbs@gmail.com>
2016-01-19 15:04:05 -0800

    add functionality to html

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,22 @@
-var triangleShape = 
+var triangleShape = function (a, b, c) {
+  console.log(a,b,c)
+
+if ((a+b<c) || (a+c<b) || (c+b<a)) {
+  return "notTriangle";
+} else if ((a ===b) && (a ===c) ) {
+  return "equilateral"
+} else if (( a === b && a !==c ) || ( a === c && a!== b) || ( b === c && b !==a)) {
+  return "isosceles"
+} else if (( a !== b && a !==c ) || ( b !== c && b !== a) || ( c !== b && c !== a)){
+  return "scalene"
+}
+};
+
+$("form").submit(function (event) {
+  var d = parseInt($(".side1").val())
+  var e = parseInt($(".side2").val())
+  var f = parseInt($(".side3").val())
+$(".printShape").text(triangleShape(d,e,f))
+event.preventDefault()
+
+})
--- spec/specs.js
+++ spec/specs.js
@@ -1,7 +1,20 @@
-describe ('triangleShape', function () {
-  it("is not a triangle if 2 sides are too small", function () {
-    expect(triangleShape(4).to.equal("none"))
+ describe ('triangleShape', function () {
+
+  it("is a triangle unless sides are too small", function () {
+    expect(triangleShape(2,3,9)).to.equal("notTriangle")
+  });
+
+  it("is an equalateral if all sides are equal", function () {
+    expect(triangleShape(4,4,4)).to.equal("equilateral")
+  });
+
+  it("is an isosceles triangle if two sides are equal", function () {
+  expect(triangleShape(4,4,6)).to.equal("isosceles")
+  });
+
+  it("is a scalene triang if no sides are equal", function () {
+    expect(triangleShape(2,3,4)).to.equal("scalene")
   });
 
 
-}
+  });
